1f4afc5bbdcb9eb88bfacd0f040359fa75dc2d57
Datta Teja, Nalluri <dattatejan@gmail.com>
2025-03-10 12:22:37 -0500

    Commit 49 Group Anagrama Leetcode

---
+++ src/GroupAnagramsLeetCode_49/GroupAnagrams.java
@@ -0,0 +1,50 @@
+package GroupAnagramsLeetCode_49;
+import java.util.*;
+
+public class GroupAnagrams {
+
+    public static List<List<String>> groupAnagrams(String[] strs){
+        List<List<String>> result = new ArrayList<>();
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String s:strs){
+            String temp = s;
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            temp = new String(arr);
+            if(!map.containsKey(temp)){
+                map.put(temp,new ArrayList<>());
+            }
+            map.get(temp).add(s);
+        }
+        System.out.println(map);
+        for(var set:map.entrySet()){
+            result.add(set.getValue());
+        }
+        return result;
+    }
+
+    //["eat","tea","tan","ate","nat","bat"]
+
+    public static List<List<String>> groupAnagramsOptimized(String[] strs){
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String s: strs){
+            int[] arr = new int[26];
+            for(char c:s.toCharArray()){
+                arr[c-'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i<26;i++){
+                sb.append(arr[i]);
+            }
+            String key = sb.toString();
+            if(!map.containsKey(key)){
+                map.put(key,new LinkedList<>());
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
-import ContainsDuplicateLeetCode_217.ContainsDuplicate;
-import ValidAnagramLeetCode_242.ValidAnagram;
+import Arrays.Array;
+import GroupAnagramsLeetCode_49.GroupAnagrams;
+import TwoSumLeetCode_1.TwoSum;
+
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(ValidAnagram.validAnagram("rat","car"));
+        System.out.println(Arrays.toString(TwoSum.twoSum(new int[]{2,7,11,15},9)));
+        GroupAnagrams.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
+        System.out.println(GroupAnagrams.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+        GroupAnagrams.groupAnagrams(new String[]{""});
         }
 
 }
