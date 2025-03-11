e4aa33e98301aa3fd054633d96b715f76b740b08
Datta Teja, Nalluri <dattatejan@gmail.com>
2025-03-11 12:25:50 -0500

    Commit 238 Product of Array Except Self Leetcode

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,13 @@
 
 
 import EncodeAndDecodeStringsLeetCode_271.EncodeAndDecodeStrings;
+import ProductOfArrayExceptSelf_238.ProductOfArrayExceptSelf;
 
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> ls = new LinkedList<>();
-        ls.add("Datta");
-        ls.add("Tej/a");
-        ls.add("Nall/:uri");
-        var str = EncodeAndDecodeStrings.encode(ls);
-        System.out.println(str);
-        System.out.println(EncodeAndDecodeStrings.decode(str));
+        ProductOfArrayExceptSelf.productOfArrayExceptSelf(new int[]{1,2,3,4});
     }
 
 }
--- src/ProductOfArrayExceptSelf_238/ProductOfArrayExceptSelf.java
+++ src/ProductOfArrayExceptSelf_238/ProductOfArrayExceptSelf.java
@@ -1,4 +1,21 @@
 package ProductOfArrayExceptSelf_238;
 
+import java.util.Arrays;
+
 public class ProductOfArrayExceptSelf {
+
+    public static int[] productOfArrayExceptSelf(int[] arr){
+        int[] result = new int[arr.length];
+        result[0] = 1;
+        for(int i = 1;i<arr.length;i++){
+            result[i] = result[i-1]*arr[i-1];
+        }
+        int R = 1;
+        for(int i = arr.length-1;i>=0;i--){
+            result[i] = result[i]*R;
+            R*=arr[i];
+        }
+        return result;
+    }
+
 }
